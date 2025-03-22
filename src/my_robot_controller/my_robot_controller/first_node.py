00f1d32b759ddf7ad7579eaad722499ebef49b84
David Maged <davidmaged1123@gmail.com>
2025-03-23 01:57:07 +0200

    setup file for my_robot_controller package

--- src/my_robot_controller/my_robot_controller/first_node.py
+++ src/my_robot_controller/my_robot_controller/first_node.py
@@ -6,7 +6,7 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__('first_node')
-        ##self.get_logger().info('Hello from ros2')
+        self.get_logger().info('Hello from ros2')
         self.counter_ = 0
         self.create_timer(1.0, self.timer_callback)
 
