84666e1cc11e93fece8c6fb86a01ba0ebe414494
David Maged <davidmaged1123@gmail.com>
2025-03-26 22:38:59 +0200

    second commit message test node and run turtle_node

--- src/launch_file/launch/node_start.launch.py
+++ src/launch_file/launch/node_start.launch.py
@@ -5,16 +5,16 @@ import launch_ros.actions
 def generate_launch_description():
     ld= LaunchDescription()
     talker_node = launch_ros.actions.Node(
-        package="demo_nodes_cpp",
-        executable="talker",
+        package="my_robot_controller",
+        executable="test_node",
         #name='talker',
         #output='screen'
     )
     listener_node = launch_ros.actions.Node(
-        package="demo_nodes_cpp",
-        executable="listener",
+        package="turtlesim",
+        executable="turtlesim_node",
         #name='listener',
-        #output='screen'
+        output='screen'
     )
 
 
